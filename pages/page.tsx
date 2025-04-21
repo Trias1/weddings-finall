4014b5a10fa1d0ef747d76a8d273d86d768ae8d1
trias <iaszaen@gmail.com>
2025-04-21 19:26:06 +0700

    update

--- pages/page.tsx
+++ pages/page.tsx
@@ -1,15 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  // Redirect ke halaman utama jika tidak ada query `name`
-  const params = new URLSearchParams(window.location.search);
-  const name = params.get("name");
-
-  if (!name) {
-    return redirect("/"); // Arahkan ke halaman utama jika tidak ada nama
-  }
-
-  // Arahkan ke halaman dinamis berdasarkan nama pengguna
-  const id = name.toLowerCase().replace(/\s+/g, "-");
-  redirect(`/trias-zulfa/undangan/${id}`);
+  redirect("/trias-zulfa/undangan");
 }
